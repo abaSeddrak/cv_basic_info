afffe95d8458b7397cae757fafe9308d9926d33f
abanoub <abanoub.anwar@sprinteye.com>
2026-04-02 18:53:29 +0200

    Fixed dependencies and added working app.py

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 import streamlit as st
-st.title("Title here")
+from PyPDF2 import PdfReader
+
+st.title("Basic CV Info")
+
+uploaded_file = st.file_uploader("Upload PDF", type="pdf")
+if uploaded_file:
+    pdf = PdfReader(uploaded_file)
+    text = ""
+    for page in pdf.pages:
+        text += page.extract_text()
+    st.text_area("PDF Content", text, height=300)
